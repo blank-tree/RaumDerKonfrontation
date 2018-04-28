506479b88f8bf916ab04637b3b5c157d42c3304e
Fernando Obieta <fernando.obieta@blanktree.ch>
2018-04-28 17:01:52 +0200

    added getValue method by H. Pauwelyn

--- Stimulation/Light/Light.ino
+++ Stimulation/Light/Light.ino
@@ -1,3 +1,12 @@
+/**
+ * Raum der Konfrontation
+ * Light // Arduino Code
+ * Controlls the lights in the room from the Max serial output 
+ * @author: Fernando Obieta & Claudio Rainolter
+ * @version: 0.03.02
+ */
+
+
 #include <Adafruit_NeoPixel.h>
 #ifdef __AVR__
 #include <avr/power.h>
@@ -57,4 +66,22 @@ void loop() {
 	light.show();
 
 	delay(SERIAL_REFRESH);
+}
+
+
+// by H. Pauwelyn
+// https://arduino.stackexchange.com/a/1237
+String getValue(String data, char separator, int index) {
+    int found = 0;
+    int strIndex[] = { 0, -1 };
+    int maxIndex = data.length() - 1;
+
+    for (int i = 0; i <= maxIndex && found <= index; i++) {
+        if (data.charAt(i) == separator || i == maxIndex) {
+            found++;
+            strIndex[0] = strIndex[1] + 1;
+            strIndex[1] = (i == maxIndex) ? i+1 : i;
+        }
+    }
+    return found > index ? data.substring(strIndex[0], strIndex[1]) : "";
 }
