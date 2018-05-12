4d9a97702bbcf7f3eff3e08d5530e2a60f4c98c1
Fernando Obieta <fernando.obieta@blanktree.ch>
2018-05-12 16:54:35 +0200

    found the bug. the settings.php is excluded from the server and needs to be uploaded manually

--- Terminal/Terminal-Web/init-db.php
+++ Terminal/Terminal-Web/init-db.php
@@ -18,8 +18,7 @@ $stmt->bind_param("ssi", $date, $time, $taken);
 
 for ($date = MIN_DATE; $date <= MAX_DATE; $date++) {
 	for ($time = MIN_TIME; $time <= MAX_TIME; $time++) {
-		$taken = $time == EXHIBITION_TIME ? TRUE : FALSE;
-		echo $taken;
+		$taken = $time == EXHIBITION_TIME;
 		$stmt->execute();
 	}
 }
