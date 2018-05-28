e288491c74a4c3d83c70852ccad282d8a768d8ba
Fernando Obieta <fernando.obieta@blanktree.ch>
2018-05-28 18:32:49 +0200

    added two decimals always to the price

--- Terminal/Terminal-Web/js/app.js
+++ Terminal/Terminal-Web/js/app.js
@@ -93,7 +93,7 @@ $(function () {
     });
 
     $('.back-button').click(function () {
-        $payment.text(price);
+        $payment.text(price.toFixed(2));
         changeState(false, timeoutRegular);
     });
 
